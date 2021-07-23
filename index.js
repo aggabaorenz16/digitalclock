c0d6e79e44aabbe7a105007e335c8d3f234bc294
aggabaorenz16 <43602006+aggabaorenz16@users.noreply.github.com>
2021-07-24 02:05:34 +1300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const clickme = document.querySelector('#name');
-
-clickme.addEventListener('click', (e) => {
-    for (let $i = 0; $i < Infinity; $i++) {
-        window.alert("Number : " + `${$i}`)
-    }
-})
